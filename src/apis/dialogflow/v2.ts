6eecdbacba9eaabdfe35b1b0d61c1aefba4d9211
yoshi-automation <yoshi-automation@google.com>
2020-06-22 04:24:37 -0700

    fix(dialogflow): update the API
    #### dialogflow:v2
    The following keys were changed:
    - resources.projects.resources.agent.methods.import.description
    - resources.projects.resources.agent.methods.restore.description
    
    #### dialogflow:v2beta1
    The following keys were changed:
    - resources.projects.resources.agent.methods.import.description
    - resources.projects.resources.agent.methods.restore.description
    - resources.projects.resources.locations.resources.agent.methods.import.description
    - resources.projects.resources.locations.resources.agent.methods.restore.description

--- src/apis/dialogflow/v2.ts
+++ src/apis/dialogflow/v2.ts
@@ -4315,7 +4315,7 @@ export namespace dialogflow_v2 {
 
     /**
      * dialogflow.projects.agent.import
-     * @desc Imports the specified agent from a ZIP file.  Uploads new intents and entity types without deleting the existing ones. Intents and entity types with the same name are replaced with the new versions from ImportAgentRequest.  Operation <response: google.protobuf.Empty>
+     * @desc Imports the specified agent from a ZIP file.  Uploads new intents and entity types without deleting the existing ones. Intents and entity types with the same name are replaced with the new versions from ImportAgentRequest. After the import, the imported draft agent will be trained automatically (unless disabled in agent settings). However, once the import is done, training may not be completed yet. Please call TrainAgent and wait for the operation it returns in order to train explicitly.  Operation <response: google.protobuf.Empty> An operation which tracks when importing is complete. It only tracks when the draft agent is updated not when it is done training.
      * @example
      * // Before running the sample:
      * // - Enable the API at:
@@ -4471,7 +4471,7 @@ export namespace dialogflow_v2 {
 
     /**
      * dialogflow.projects.agent.restore
-     * @desc Restores the specified agent from a ZIP file.  Replaces the current agent version with a new one. All the intents and entity types in the older version are deleted.  Operation <response: google.protobuf.Empty>
+     * @desc Restores the specified agent from a ZIP file.  Replaces the current agent version with a new one. All the intents and entity types in the older version are deleted. After the restore, the restored draft agent will be trained automatically (unless disabled in agent settings). However, once the restore is done, training may not be completed yet. Please call TrainAgent and wait for the operation it returns in order to train explicitly.  Operation <response: google.protobuf.Empty> An operation which tracks when restoring is complete. It only tracks when the draft agent is updated not when it is done training.
      * @example
      * // Before running the sample:
      * // - Enable the API at:
